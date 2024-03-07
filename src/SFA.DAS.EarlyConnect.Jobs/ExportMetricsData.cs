379bc2bba75306e3a60f7c54fe08d5a5a0f74134
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-03-07 16:59:36 +0000

    EC-397-Export container name changed

--- src/SFA.DAS.EarlyConnect.Jobs/ExportMetricsData.cs
+++ src/SFA.DAS.EarlyConnect.Jobs/ExportMetricsData.cs
@@ -28,7 +28,7 @@ namespace SFA.DAS.EarlyConnect.Functions
         {
             _metricsDataBulkDownloadHandler = metricsDataBulkDownloadHandler;
             _blobService = blobService;
-            _exportContainer = configuration["Containers:ExportContainer"];
+            _exportContainer = configuration["Containers:MetricsDataExportContainer"];
             _getLEPSDataWithUsersHandler = getLEPSDataWithUsersHandler;
         }
 
