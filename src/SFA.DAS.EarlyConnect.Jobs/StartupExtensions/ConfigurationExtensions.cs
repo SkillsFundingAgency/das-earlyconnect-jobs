1cc8e630ef2775900a56fa07d38cd913fb105502
David Rimar <david.rimar@digital.education.gov.uk>
2024-08-20 15:31:02 +0100

    change program cs

---
+++ src/SFA.DAS.EarlyConnect.Jobs/StartupExtensions/ConfigurationExtensions.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using SFA.DAS.Configuration.AzureTableStorage;
+using System.IO;
+
+namespace SFA.DAS.EarlyConnect.Jobs.StartupExtensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static IConfiguration BuildDasConfiguration(this IConfigurationBuilder configBuilder, IConfiguration configuration)
+        {
+            configBuilder
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddEnvironmentVariables();
+
+#if DEBUG
+            configBuilder.AddJsonFile("local.settings.json", optional: true);
+#endif
+            configBuilder.AddAzureTableStorage(options =>
+            {
+                options.ConfigurationKeys = ["SFA.DAS.EarlyConnect.Jobs"];
+                options.StorageConnectionString = configuration["ConfigurationStorageConnectionString"];
+                options.EnvironmentName = configuration["EnvironmentName"];
+                options.PreFixConfigurationKeys = false;
+            });
+
+            return configBuilder.Build();
+        }
+    }
+}
